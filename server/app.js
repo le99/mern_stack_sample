76b60e128699aab5c89ba145d2bc3eea6e349a40
lef <le.florez602@uniandes.edu.co>
2020-10-23 10:55:09 -0500

    Removed Jade

--- server/app.js
+++ server/app.js
@@ -9,10 +9,6 @@ var apiRouter = require('./routes/api');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -34,12 +30,12 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  let message = err.message;
+  let error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({'error': {status: error.status, stack: error.stack}});
 });
 
 module.exports = app;
